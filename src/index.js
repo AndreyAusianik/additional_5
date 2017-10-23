31824eaa02caaf1b5413ca66b0b10aac49bd009b
Andrei Ausianik <andrei.ausianik@iii.com>
2017-10-23 16:51:16 +0300

    brackets problem solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const stack = [];
+  stack.top = () => stack[stack.length - 1];
+
+  const bracketsMap = {};
+  bracketsConfig.forEach(([open, close]) => bracketsMap[open] = close);
+  for(const char of str) {
+      if(char === stack.top()) {
+          stack.pop();
+          continue;
+      }
+
+      if(!bracketsMap[char]) {
+          return false;
+      }
+
+      stack.push(bracketsMap[char]);
+  }
+
+  return stack.length === 0;
+};
